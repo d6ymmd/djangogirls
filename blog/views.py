32208c189781526712db5ccbc288682a84246ace
d6ymmd <d6ymmd@gmail.com>
2019-12-11 15:42:48 +0900

    Template를 찾을 경로를 settings.TEMPLATE에 정하고, post_list view에서는 render()함수를 사용해서 템플릿 파일을 text로 변환하고 응답하도록 함

--- blog/views.py
+++ blog/views.py
@@ -1,49 +1,7 @@
-import os
-from django.http import HttpResponse
 from django.shortcuts import render
 
-
 def post_list(request):
-
-    # 상위폴더(blog)의
-    #  상위폴더 (djangogirls)의
-    #   하위폴더 (templates)의
-    #   하위파일(post_list.html)의 내용을 read()한 결과를 HttpResponse에 인자로 전달
-
-    # 경로이동
-    # os.path.abspath(__file__) <-현재 파일의 절대 경로를 리턴해줌
-    # os.path.dirname
-    # os.path.join
-
-    # 파일열기
-    # open
-
-    cur_file_path = os.path.abspath(__file__)
-    blog_dir_path = os.path.dirname(cur_file_path)
-
-    root_dir_path = os.path.dirname(blog_dir_path)
-    templates_dir_path = os.path.join(root_dir_path, 'templates')
-    post_list_html_path = os.path.join(templates_dir_path, 'post_list.html')
-
-    f = open(post_list_html_path, 'rt')
-    html = f.read()
-    f.close()
-
-
-    return HttpResponse(html)
-        # '<html>'
-        # '<body>'
-        # '<h1>Post List!</h1>'
-        # '<div>'
-        # '<p>published: 12.11.2019, 14:38</p>'
-        # '<h2><a href="">My First post</a></h2>'
-        # '<p>asdfasdfasdfasdf</p>'
-        # '</div>'
-        # '<div>'
-        # '<p>published: 12.11.2019, 14:39</p>'
-        # '<h2><a href="">My Second post</a></h2>'
-        # '<p>asdfasdfasdfasdf</p>'
-        # '</div>'
-        # '</body>'
-        # '</html>')
-
+    # Template을 찾을 경로에서 수
+    # post_list.html을 찾아서 그 파일을 text로 만들어서
+    # HttpResponse의 형태로 돌려준다 위기능을 하는 shortcut함
+    return render(request, 'post_list.html')
